2ab15a0883a4eb401f26fe2cb056ba9ec28785f5
mcdominik <dominik8881@wp.pl>
2023-06-02 00:42:15 +0200

    fix avatar routing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -19,12 +19,14 @@ import { Language, useAuthStore } from "../zustand";
 import { useAuth } from "../hooks/useAuth";
 import { Path } from "../pages/Paths";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
 
 export const MenuItems = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <>
+
       <Flag locale={Language.PL} src="../flag-pl.png" />
       <Flag locale={Language.EN} src="../flag-en.png" />
       <Button onClick={toggleColorMode}>
@@ -38,16 +40,16 @@ export default function Nav() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const user = useAuthStore((state) => state.user);
   const { logout } = useAuth();
+  const navigate = useNavigate()
 
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
+          <IconButton      
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
             aria-label={"Open Menu"}
-            display={{ md: "none" }}
+            display={{ md: "none"}}
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={"center"} w={"100%"}>
@@ -68,6 +70,7 @@ export default function Nav() {
               <>
                 <Menu>
                   <MenuButton
+                    onClick={() => {navigate(Path.MY_PROFILE)}}
                     ml={8}
                     as={Button}
                     rounded={"full"}
@@ -77,7 +80,6 @@ export default function Nav() {
                   >
                     <Avatar
                       as={"a"}
-                      href={Path.MY_PROFILE}
                       size={"sm"}
                       src={
                         "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
@@ -124,7 +126,7 @@ export default function Nav() {
 
         {isOpen ? (
           <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
+            <Stack as={"nav"} spacing={3}>
               <MenuItems />
             </Stack>
           </Box>

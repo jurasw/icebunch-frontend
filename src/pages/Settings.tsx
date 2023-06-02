4e0a64607d8c0031277c535328e044b7b1c25dbc
mcdominik <dominik8881@wp.pl>
2023-06-02 12:49:14 +0200

    fix imports

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3,12 +3,12 @@ import { Center, Text } from "@chakra-ui/react";
 // import { useEffect, useState } from "react";
 // import axios from "axios";
 // import { IceCream } from "../models/IceCream";
-import { useAuthStore } from "../zustand";
+// import { useAuthStore } from "../zustand";
 import Nav from "../components/Nav";
 
 const Settings = () => {
   //   const [iceCream, setIceCream] = useState<IceCream[]>([]);
-  const user = useAuthStore((state) => state.user);
+  // const user = useAuthStore((state) => state.user);
 
   return (
     <>

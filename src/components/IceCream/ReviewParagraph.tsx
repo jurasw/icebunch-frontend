d82ff4c5e23b28df479ab52ca9140e9e91583296
mcdominik <dominik8881@wp.pl>
2023-06-02 16:22:43 +0200

    fix avatar display

--- src/components/IceCream/ReviewParagraph.tsx
+++ src/components/IceCream/ReviewParagraph.tsx
@@ -1,37 +1,19 @@
-import { Avatar, Box } from "@chakra-ui/react";
 import { Review } from "../../models/Review";
-import RatingWithoutCounter from "./RatingWithoutCounter";
-import { useNavigate } from "react-router-dom";
+import OneReview from "./OneReview";
 
 interface ReviewProps {
   reviews: Review[] | undefined;
-  // profilePictureUrl: string'
-
+  avatarUrl: string;
 }
 
 function ReviewParagraph(props: ReviewProps) {
 
-  const navigate = useNavigate()
-
   return (
     <>
       {props.reviews?.map((review) => (
-        <Box
-        borderWidth='1px'
-        borderRadius='lg'
-        overflow='hidden'
-        margin={'0.5em'}
-        padding={'1em'}
-        _hover={{cursor:"pointer"}}
-        onClick={() => {
-          navigate(`/profile/${review.userId}`)
-        }}
-        >
-          <Avatar name="testowy user" src="https://bit.ly/dan-abramov" />
-          {review?.username}
-          <RatingWithoutCounter rating={review.rating} />
-          <p>{review.content}</p>
-          </Box>
+        <OneReview
+        review={review}
+        />
       ))}
     </>
   );

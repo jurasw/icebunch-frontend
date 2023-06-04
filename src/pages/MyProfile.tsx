d23c6f4aadb7a4e049e6d0d29e33b2de6d67c044
jurasw <juraswis@gmail.com>
2023-06-04 22:02:41 +0200

    my profile avatar fix

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,30 +1,28 @@
 import { Center, Text, Button, Avatar, VStack } from "@chakra-ui/react";
-// import { SearchIcon } from "@chakra-ui/icons";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import { IceCream } from "../models/IceCream";
 import { useAuthStore } from "../zustand";
 import Nav from "../components/Nav";
 import { useNavigate } from "react-router-dom";
 import { Path } from "./Paths";
 import { useUser } from "../hooks/queries/useUser";
 import { useEffect, useState } from "react";
+import { UserDB } from "../models/User";
 
 const MyProfile = () => {
-  const navigate = useNavigate()
-  //   const [iceCream, setIceCream] = useState<IceCream[]>([]);
-  const [avatarUrl, setAvatarUrl] = useState('')
+  const navigate = useNavigate();
+
   const user = useAuthStore((state) => state.user);
+  const { getUserFromEmail } = useUser();
+
+  const [userData, setUserData] = useState<UserDB>();
 
   useEffect(() => {
-    const fetchAvatar = async () => {
+    const fetchUserData = async () => {
       if (user) {
-      const { getUserAvatarFromEmail } = useUser();
-      const result = await getUserAvatarFromEmail(user.email)
-      setAvatarUrl(result);
-    }
+        const result = await getUserFromEmail(user.email);
+        setUserData(result);
+      }
     };
-    fetchAvatar();
+    fetchUserData();
   }, []);
 
   return (
@@ -32,21 +30,17 @@ const MyProfile = () => {
       <Nav />
       <Center margin={8}>
         <VStack spacing={8}>
-        <Text fontSize={"4xl"} as="b">
-          Welcome {user?.email}
-        </Text>
-      <Avatar
-                      as={"a"}
-                      size={"2xl"}
-                      src={avatarUrl
-                      }
-                    />
-        <Button
-        onClick={() => {navigate(Path.SETTINGS)}}
-        
-        >
+          <Text fontSize={"4xl"} as="b">
+            Welcome {user?.email}
+          </Text>
+          <Avatar as={"a"} size={"2xl"} src={userData?.avatarUrl} />
+          <Button
+            onClick={() => {
+              navigate(Path.SETTINGS);
+            }}
+          >
             Settings
-        </Button>
+          </Button>
         </VStack>
       </Center>
     </>

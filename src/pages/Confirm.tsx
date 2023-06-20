016bda76f9e6b3add230b1ce748d9df03b3a7c30
jurasw <juraswis@gmail.com>
2023-06-20 14:03:54 +0200

    change confirm pages

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -29,58 +29,59 @@ export default function Confirm(): JSX.Element {
   }, []);
 
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Stack
-        spacing={4}
-        w={"full"}
-        maxW={"sm"}
-        bg="background"
-        rounded={"xl"}
-        boxShadow={"lg"}
-        p={6}
-        my={10}
+    <>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
       >
-        {confirmed ? (
-          <>
-            <Center>
-              <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
-                {t('email-confirmed')}
-              </Heading>
-            </Center>
-            <Center
-              fontSize={{ base: "sm", sm: "md" }}
-              color={useColorModeValue("gray.800", "gray.400")}
-            >
-              {t('confirmed-message')}
-    
-            </Center>
-            <Stack spacing={6}>
-              <Button as={"a"} href={Path.ICE_CREAMS}>
-                {t('go-to-app')}
-              </Button>
-            </Stack>
-          </>
-        ) : (
-          <>
-            <Center>
-              <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
-                {t('confirmation-failed')}
-              </Heading>
-            </Center>
-            <Center
-              fontSize={{ base: "sm", sm: "md" }}
-              color={useColorModeValue("gray.800", "gray.400")}
-            >
-              {t('confirmation-failed-message')}
-            </Center>
-          </>
-        )}
-      </Stack>
-    </Flex>
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"sm"}
+          bg="background"
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={10}
+        >
+          {confirmed ? (
+            <>
+              <Center>
+                <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+                  {t("email-confirmed")}
+                </Heading>
+              </Center>
+              <Center
+                fontSize={{ base: "sm", sm: "md" }}
+                color={useColorModeValue("gray.800", "gray.400")}
+              >
+                {t("confirmed-message")}
+              </Center>
+              <Stack spacing={6}>
+                <Button as={"a"} href={Path.LOGIN}>
+                  {t("go-to-app")}
+                </Button>
+              </Stack>
+            </>
+          ) : (
+            <>
+              <Center>
+                <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+                  {"confirmation-failed"}
+                </Heading>
+              </Center>
+              <Center
+                fontSize={{ base: "sm", sm: "md" }}
+                color={useColorModeValue("gray.800", "gray.400")}
+              >
+                {"confirmation-failed-message"}
+              </Center>
+            </>
+          )}
+        </Stack>
+      </Flex>
+    </>
   );
 }
--- src/pages/ConfirmRequest.tsx
+++ src/pages/ConfirmRequest.tsx
@@ -1,28 +1,41 @@
-import { Center, Text } from "@chakra-ui/react";
+import { Center, Heading, Stack } from "@chakra-ui/react";
 import { Flex, useColorModeValue } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-
 
 export default function ConfirmRequest(): JSX.Element {
   const { t } = useTranslation();
 
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-          <>
-            <Center
-            >
-            <Text
-            size={'2xl'}
-            >
-              {t('email-confirm')}
-                </Text>
-            </Center>
-          </>
-    </Flex>
+    <>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"sm"}
+          bg={useColorModeValue("white", "gray.700")}
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={10}
+        >
+          <Center>
+            <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+              {t("email-confirm-title")}
+            </Heading>
+          </Center>
+          <Center
+            fontSize={{ base: "sm", sm: "md" }}
+            color={useColorModeValue("gray.800", "gray.400")}
+          >
+            {t("email-confirm")}
+          </Center>
+        </Stack>
+      </Flex>
+    </>
   );
 }
